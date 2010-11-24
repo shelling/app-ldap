dff40d8aba465616f205fe908be6f30a92f3b8da
shelling <navyblueshellingford@gmail.com>
2010-11-24 15:11:44 +0800

    initial commit

---
+++ lib/App/LDAP.pm
@@ -0,0 +1,32 @@
+package App::LDAP;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+App::LDAP -
+
+=head1 SYNOPSIS
+
+  use App::LDAP;
+
+=head1 DESCRIPTION
+
+App::LDAP is
+
+=head1 AUTHOR
+
+shelling E<lt>navyblueshellingford@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
