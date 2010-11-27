e878405b1bb736122023992d298e3ab90e3a2e3a
shelling <navyblueshellingford@gmail.com>
2010-11-27 18:57:57 +0800

    remove trivial code for bug-fixed App::CLI

--- lib/App/LDAP/Command/Add.pm
+++ lib/App/LDAP/Command/Add.pm
@@ -6,8 +6,6 @@ package App::LDAP::Command::Add;
 use base qw(App::CLI::Command);
 use constant subcommands => qw(User);
 
-use App::LDAP::Command::Add::User; # should be deleted
-
 sub run {
   my ($self,) = @_;
   say "command->add";
--- lib/App/LDAP/Command/Del.pm
+++ lib/App/LDAP/Command/Del.pm
@@ -6,8 +6,6 @@ package App::LDAP::Command::Del;
 use base qw(App::CLI::Command);
 use constant subcommands => qw(User);
 
-use App::LDAP::Command::Del::User; # should be deleted
-
 sub run {
   my ($self,) = @_;
   say "command->del";
