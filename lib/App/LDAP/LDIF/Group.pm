3b49ba97bf16670bb842a45c13d3228ff71eb0ed
shelling <navyblueshellingford@gmail.com>
2012-03-25 01:18:22 +0800

    implement LDIF::Group

--- lib/App/LDAP/LDIF/Group.pm
+++ lib/App/LDAP/LDIF/Group.pm
@@ -1,8 +1,90 @@
-use 5.010;
-use strict;
-use warnings;
-
 package App::LDAP::LDIF::Group;
 
+use Moose;
+
+use Net::LDAP::Entry;
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $args = {@_};
+    my $ou   = $args->{ou};
+    my $name = $args->{name};
+    my $id   = $args->{id};
+
+    $self->$orig(
+        dn        => "cn=$name,$ou",
+        cn        => $name,
+        gidNumber => $id,
+    );
+
+};
+
+has dn => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+has objectClass => (
+    is      => "rw",
+    isa     => "ArrayRef[Str]",
+    default => sub {
+        [
+            qw( posixGroup
+                top )
+        ]
+    },
+);
+
+has cn => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+has userPassword => (
+    is      => "rw",
+    isa     => "Str",
+    default => "{crypt}x",
+);
+
+
+has gidNumber => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+sub entry {
+    my ($self) = shift;
+
+    my $entry = Net::LDAP::Entry->new( $self->dn );
+
+    for (qw( objectClass
+             cn
+             userPassword
+             gidNumber ))
+    {
+        $entry->add($_ => $self->{$_});
+    }
+
+    $entry;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 
 1;
+
+=pod
+
+=head1 NAME
+
+App::LDAP::LDIF::Group - the presentation of groups in LDAP
+
+=head1 SYNOPSIS
+
+
+=cut
---
+++ t/08_ldif_group.t
@@ -0,0 +1,51 @@
+use Modern::Perl;
+use Test::More;
+
+use App::LDAP::LDIF::Group;
+
+my $group = App::LDAP::LDIF::Group->new(
+    ou   => "ou=Group,dc=example,dc=com",
+    name => "nobody",
+    id   => 1001,
+);
+
+is (
+    $group->dn,
+    "cn=nobody,ou=Group,dc=example,dc=com",
+    "dn is compose of name and ou",
+);
+
+is_deeply (
+    $group->objectClass,
+    [qw(posixGroup top)],
+    "objectClass has default value",
+);
+
+is (
+    $group->cn,
+    "nobody",
+    "cn is name",
+);
+
+is (
+    $group->userPassword,
+    "{crypt}x",
+    "userPassword has default value",
+);
+
+is (
+    $group->entry->ldif,
+<<LDIF
+
+dn: cn=nobody,ou=Group,dc=example,dc=com
+objectClass: posixGroup
+objectClass: top
+cn: nobody
+userPassword: {crypt}x
+gidNumber: 1001
+LDIF
+,
+    "$group->entry() provide the same order as openldap utils",
+);
+
+done_testing;
