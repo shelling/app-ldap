7c3bd81941a6f99b4a5d91e050de4e836b6654d0
shelling <navyblueshellingford@gmail.com>
2012-03-31 01:05:01 +0800

    implement LDIF::Sudoer

--- lib/App/LDAP/LDIF/Sudoer.pm
+++ lib/App/LDAP/LDIF/Sudoer.pm
@@ -1,8 +1,88 @@
-use 5.010;
-use strict;
-use warnings;
-
 package App::LDAP::LDIF::Sudoer;
 
+use Moose;
+
+use Net::LDAP::Entry;
+
+around BUILDARGS => sub {
+    my $orig = shift;
+    my $self = shift;
+
+    my $args = {@_};
+    my $ou   = $args->{ou};
+    my $name = $args->{name};
+
+    $self->$orig(
+        dn       => "cn=$name,$ou",
+        cn       => $name,
+        sudoUser => $name,
+    );
+
+};
+
+has dn => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+has objectClass => (
+    is      => "rw",
+    isa     => "ArrayRef[Str]",
+    default => sub {
+        [
+            qw( top
+                sudoRole )
+        ]
+    },
+);
+
+has cn => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+has sudoUser => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+has sudoHost => (
+    is      => "rw",
+    isa     => "Str",
+    default => "ALL",
+);
+
+has sudoRunAsUser => (
+    is      => "rw",
+    isa     => "Str",
+    default => "ALL",
+);
+
+has sudoCommand => (
+    is      => "rw",
+    isa     => "Str",
+    default => "ALL",
+);
+
+sub entry {
+    my ($self) = shift;
+    my $entry = Net::LDAP::Entry->new( $self->dn );
+
+    $entry->add($_ => $self->$_)
+      for qw( objectClass
+              cn
+              sudoUser
+              sudoHost
+              sudoRunAsUser
+              sudoCommand );
+
+    $entry;
+}
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
 
 1;
---
+++ t/10_ldif_sudoer.t
@@ -0,0 +1,68 @@
+use Modern::Perl;
+use Test::More;
+
+use App::LDAP::LDIF::Sudoer;
+
+my $sudoer = App::LDAP::LDIF::Sudoer->new(
+    ou => "ou=Sudoer,dc=example,dc=com",
+    name => "first",
+);
+
+is (
+    $sudoer->dn,
+    "cn=first,ou=Sudoer,dc=example,dc=com",
+    "dn is composed of name and ou",
+);
+
+is_deeply (
+    $sudoer->objectClass,
+    [qw(top sudoRole)],
+    "objectClass has default value",
+);
+
+is (
+    $sudoer->cn,
+    "first",
+    "cn is name",
+);
+
+is (
+    $sudoer->sudoUser,
+    "first",
+    "sudoUser is name",
+);
+
+is (
+    $sudoer->sudoHost,
+    "ALL",
+    "sudoHost has default value ALL",
+);
+
+is (
+    $sudoer->sudoRunAsUser,
+    "ALL",
+    "sudoRunAsUser has default value ALL",
+);
+
+is (
+    $sudoer->sudoCommand,
+    "ALL",
+    "sudoCommand has default value ALL",
+);
+
+is (
+    $sudoer->entry->ldif,
+<<LDIF
+
+dn: cn=first,ou=Sudoer,dc=example,dc=com
+objectClass: top
+objectClass: sudoRole
+cn: first
+sudoUser: first
+sudoHost: ALL
+sudoRunAsUser: ALL
+sudoCommand: ALL
+LDIF
+);
+
+done_testing;
