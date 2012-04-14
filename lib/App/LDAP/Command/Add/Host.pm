c68b8e17c11d6f834c2eb5fa25bd42c405101711
shelling <navyblueshellingford@gmail.com>
2012-04-14 17:24:00 +0800

    implement Add::Host

--- lib/App/LDAP/Command/Add/Host.pm
+++ lib/App/LDAP/Command/Add/Host.pm
@@ -8,7 +8,36 @@ use Moose;
 
 with 'MooseX::Getopt';
 
+has base => (
+    is  => "rw",
+    isa => "Str",
+);
+
+
+use Term::Prompt;
+
+use App::LDAP::LDIF::Host;
+
 sub run {
+    my ($self, $app) = @_;
+
+    my $ldap   = $app->ldap;
+    my $config = $app->config;
+
+    my $hostname = $ARGV[2] or die "no hostname specified";
+
+    my ($base, $scope) = split /\?/, $config->{nss_base_hosts};
+    $base = $self->base // $base;
+
+    my $ip = prompt('x', 'ip address:', '', '');
+
+    my $host = App::LDAP::LDIF::Host->new(
+        ou   => $base,
+        name => $hostname,
+        ip   => $ip,
+    );
+
+    my $msg = $ldap->add($host->entry); die $msg->error if $msg->code;
 
 }
 
