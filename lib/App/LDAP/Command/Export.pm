cfb81e3e879f6a69b47b96fc5a8a6cdbe8fbdd61
shelling <navyblueshellingford@gmail.com>
2012-04-14 15:10:32 +0800

    implement Command::Export

--- lib/App/LDAP/Command/Export.pm
+++ lib/App/LDAP/Command/Export.pm
@@ -8,12 +8,70 @@ use Moose;
 
 with 'MooseX::Getopt';
 
+has base => (
+    is  => "rw",
+    isa => "Str",
+);
+
+has scope => (
+    is  => "rw",
+    isa => "Str",
+);
+
+has filter => (
+    is  => "rw",
+    isa => "Str",
+);
+
 sub run {
-  my ($self,) = @_;
-  say "command->export";
+    my ($self, $app) = @_;
+    my $ldap   = $app->ldap;
+    my $config = $app->config;
+
+    my $file = $ARGV[1];
+
+    if (! defined($file)) {
+        say "you must give the file name to export";
+        exit;
+    }
+
+    my $base   = $self->base   // $config->{base};
+    my $scope  = $self->scope  // $config->{scope};
+    my $filter = $self->filter // "objectClass=*";
+
+    my @entries = $ldap->search(
+        base   => $base,
+        scope  => $scope,
+        filter => $filter,
+    )->entries;
+
+
+    open my $output, ">", $file or die "can not open $file";
+    for (@entries) {
+        print $output $_->ldif;
+    }
+
 }
+
 
 __PACKAGE__->meta->make_immutable;
 no Moose;
 
 1;
+
+=head1 NAME
+
+App::LDAP::Command::Export
+
+=head1 SYNOPSIS
+
+    $ sudo ldap export backup.ldif
+    backup whole DIT
+
+    $ ldap export people.ldif --base ou=people,dc=example,dc=com
+    dump user information without password
+
+    $ sudo ldap export people.ldif --base ou=people,dc=example,dc=com
+    dump user information with password
+
+=cut
