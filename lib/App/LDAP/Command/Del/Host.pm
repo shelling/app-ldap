f8e2bcfa23917b4eacf9c924513a2618705625c4
shelling <navyblueshellingford@gmail.com>
2012-04-21 11:19:14 +0800

    implement del host

--- lib/App/LDAP/Command/Del/Host.pm
+++ lib/App/LDAP/Command/Del/Host.pm
@@ -9,6 +9,27 @@ use Moose;
 with 'MooseX::Getopt';
 
 sub run {
+    my ($self, $app) = @_;
+
+    my $ldap   = $app->ldap;
+    my $config = $app->config;
+
+    my $hostname = $ARGV[2] or die "no hostname specified";
+
+    my ($base, $scope) = split /\?/, $config->{nss_base_hosts};
+
+    my $result = $ldap->search(
+        base   => $base,
+        scope  => $scope,
+        filter => "cn=$hostname",
+    );
+
+    if ($result->count) {
+        $ldap->delete($result->entry(0)->dn);
+        say "host $hostname has been delete";
+    } else {
+        say "host $hostname not found";
+    }
 
 }
 
