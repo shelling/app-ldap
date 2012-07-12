f116fd42398c6297df8a0bffd18aed10d84f13b6
shelling <navyblueshellingford@gmail.com>
2012-07-13 00:19:14 +0800

    implement Command/Passwd.pm

--- lib/App/LDAP/Command/Passwd.pm
+++ lib/App/LDAP/Command/Passwd.pm
@@ -6,11 +6,43 @@ use Namespace::Dispatch;
 
 use Moose;
 
+use App::LDAP::Utils;
+
 with 'MooseX::Getopt';
 
 sub run {
-  my ($self,) = @_;
-  say "command->passwd";
+    my ($self,) = @_;
+
+    if ( $< == 0 ) {
+        $ARGV[1]
+        ? change_password(find_user_by_name($ARGV[1]))
+        : change_password(current_user);
+    } else {
+        $ARGV[1]
+        ? die "you may not view or modify password information for ".$ARGV[1]
+        : change_password(current_user);
+    }
+}
+
+sub change_password {
+    my $user = shift;
+    $user->replace(
+        userPassword => encrypt(new_password())
+    )->update(App::LDAP->instance->ldap);
+}
+
+sub find_user_by_name {
+    my $name = shift;;
+    my $search = App::LDAP->instance->ldap->search(
+        base   => config->{nss_base_passwd}->[0],
+        scope  => config->{nss_base_passwd}->[1],
+        filter => "uid=$name",
+    );
+    if ($search->count > 0) {
+        return $search->entry(0);
+    } else {
+        die "user $name not found";
+    }
 }
 
 __PACKAGE__->meta->make_immutable;
