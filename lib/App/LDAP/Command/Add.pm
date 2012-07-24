10c2136821f94733fe2266f2c70232a50d3422ca
shelling <navyblueshellingford@gmail.com>
2012-07-24 16:03:04 +0800

    A stem command shows its submodules

--- lib/App/LDAP/Command/Add.pm
+++ lib/App/LDAP/Command/Add.pm
@@ -4,11 +4,11 @@ use Modern::Perl;
 
 use Moose;
 
-with 'App::LDAP::Role::Command';
+with qw( App::LDAP::Role::Command
+         App::LDAP::Role::Stem );
 
 sub run {
-    my ($self,) = @_;
-    ...
+    my ($self, ) = @_;
 }
 
 __PACKAGE__->meta->make_immutable;
