42760bcc982f68be2f9dccefd52a8efa0f14826f
shelling <navyblueshellingford@gmail.com>
2012-09-15 18:25:36 +0800

    implement posixGroup

---
+++ lib/App/LDAP/ObjectClass/PosixGroup.pm
@@ -0,0 +1,44 @@
+package App::LDAP::ObjectClass::PosixGroup;
+
+use Modern::Perl;
+
+use Moose;
+
+extends qw(App::LDAP::ObjectClass::Top);
+
+has [qw(cn gidNumber)] => (
+    is       => "rw",
+    isa      => "Str",
+    required => 1,
+);
+
+has [qw(userPassword memberUid description)] => (
+    is  => "rw",
+    isa => "Str",
+);
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+=pod
+
+=head1 NAME
+
+App::LDAP::ObjectClass::PosixGroup - schema of posixGroup
+
+=head1 DEFINITION
+
+    objectclass (
+        1.3.6.1.1.1.2.2
+        NAME 'posixGroup'
+        DESC 'Abstraction of a group of accounts'
+        SUP top
+        STRUCTURAL
+        MUST ( cn $ gidNumber )
+        MAY ( userPassword $ memberUid $ description )
+    )
+
+=cut
+
