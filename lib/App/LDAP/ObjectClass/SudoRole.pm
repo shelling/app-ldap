03dab159b59745e192386e8987ab3524dd6e722e
shelling <navyblueshellingford@gmail.com>
2012-09-17 00:50:21 +0800

    implement objectClass sudoRole

---
+++ lib/App/LDAP/ObjectClass/SudoRole.pm
@@ -0,0 +1,51 @@
+package App::LDAP::ObjectClass::SudoRole;
+
+use Modern::Perl;
+
+use Moose;
+
+extends qw(App::LDAP::ObjectClass::Top);
+
+has cn => (
+    is       => "rw",
+    isa      => "ArrayRef[Str]",
+    required => 1,
+);
+
+has [qw( sudoUser
+         sudoHost
+         sudoCommand
+         sudoRunAs
+         sudoRunAsUser
+         sudoRunAsGroup
+         sudoOption
+         description )] => (
+    is  => "rw",
+    isa => "Str",
+);
+
+__PACKAGE__->meta->make_immutable;
+no Moose;
+
+1;
+
+=pod
+
+=head1 NAME
+
+App::LDAP::ObjectClass::SudoRole - schema of sudoRole
+
+=head1 DEFINITION
+
+    objectclass (
+        1.3.6.1.4.1.15953.9.2.1
+        NAME 'sudoRole'
+        SUP top
+        STRUCTURAL
+        DESC 'Sudoer Entries'
+        MUST ( cn )
+        MAY ( sudoUser $ sudoHost $ sudoCommand $ sudoRunAs $
+              sudoRunAsUser $ sudoRunAsGroup $ sudoOption $ description )
+    )
+
+=cut
